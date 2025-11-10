92526fd1f44052187d14a5a6e2f9a3199f779cda
Otech <otengebenezer326@gmail.com>
2025-11-10 06:51:26 -0800

    Update Popular.jsx

--- src/Pages/Popular.jsx
+++ src/Pages/Popular.jsx
@@ -95,7 +95,7 @@ const Popular = () => {
         ) : (
           <>
             <motion.div
-              className="columns-1 sm:columns-2 md:columns-3 xl:columns-4 gap-6 space-y-6"
+              className="columns-2 sm:columns-2 md:columns-3 xl:columns-4 gap-6 space-y-6"
               initial="hidden"
               animate="visible"
               variants={{ visible: { transition: { staggerChildren: 0.1 } } }}
